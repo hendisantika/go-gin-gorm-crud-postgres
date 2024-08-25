3d8707a63d30ebd429ed55b392325285bab74846
hendisantika <hendisantika@yahoo.co.id>
2024-08-25 07:40:07 +0700

    feat: Add Create🫸🌀✏️📗🐧🐳

--- service/tags_service_impl.go
+++ service/tags_service_impl.go
@@ -1,6 +1,11 @@
 package service
 
-import "go-gin-gorm-crud-postgres/repository"
+import (
+	"go-gin-gorm-crud-postgres/data/request"
+	"go-gin-gorm-crud-postgres/helper"
+	"go-gin-gorm-crud-postgres/model"
+	"go-gin-gorm-crud-postgres/repository"
+)
 
 type TagsServiceImpl struct {
 	TagsRepository repository.TagsRepository
@@ -12,4 +17,14 @@ func NewTagsServiceImpl(tagRepository repository.TagsRepository, validate *valid
 		TagsRepository: tagRepository,
 		Validate:       validate,
 	}
+}
+
+// Create implements TagsService
+func (t *TagsServiceImpl) Create(tags request.CreateTagsRequest) {
+	err := t.Validate.Struct(tags)
+	helper.ErrorPanic(err)
+	tagModel := model.Tags{
+		Name: tags.Name,
+	}
+	t.TagsRepository.Save(tagModel)
 }

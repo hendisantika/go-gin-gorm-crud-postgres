4c830bd7ea4e9dd186be8224a97bddc6dcdde99f
hendisantika <hendisantika@yahoo.co.id>
2024-08-25 07:37:29 +0700

    feat: Add Delete 🫸🌀✏️📗🐧🐳

--- repository/tags_repository_impl.go
+++ repository/tags_repository_impl.go
@@ -1,6 +1,10 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"go-gin-gorm-crud-postgres/helper"
+	"go-gin-gorm-crud-postgres/model"
+	"gorm.io/gorm"
+)
 
 type TagsRepositoryImpl struct {
 	Db *gorm.DB
@@ -8,4 +12,11 @@ type TagsRepositoryImpl struct {
 
 func NewTagsRepositoryImpl(Db *gorm.DB) TagsRepository {
 	return &TagsRepositoryImpl{Db: Db}
+}
+
+// Delete implements TagsRepository
+func (t *TagsRepositoryImpl) Delete(tagsId int) {
+	var tags model.Tags
+	result := t.Db.Where("id = ?", tagsId).Delete(&tags)
+	helper.ErrorPanic(result.Error)
 }
